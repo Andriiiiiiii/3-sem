c44033269198eb0cc60fbf6d4c306e51e5c66bf4
Andriiiiiiii <91418387+Andriiiiiiii@users.noreply.github.com>
2022-10-22 15:01:58 +0300

    Add files via upload

---
+++ lab03/lab03.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+using State = int;
+
+class MainState 
+{
+public:
+    virtual bool contains(State s) const = 0;
+    //virtual ~MainState() = default;
+};
+
+// copy
+class DiscreteState : public MainState 
+{
+private:
+    State const s0;
+public:
+    DiscreteState(State s0) : s0(s0) {}
+    bool contains(State s) const override 
+    {
+        return s == s0;
+    }
+};
+
+// copy 
+class SegmentState : public MainState 
+{
+private:
+    State const begin_s0, end_s0;
+public:
+    SegmentState(State begin_s0, State end_s0) :
+        begin_s0(begin_s0), end_s0(end_s0) {}
+    bool contains(State s) const override 
+    {
+        return begin_s0 <= s and end_s0 >= s;
+    }
+};
+
+
+class ProbabilityTest 
+{
+private:
+    State const E_min, E_max;
+
+public:
+    ProbabilityTest(State E_min, State E_max):
+        E_min(E_min), E_max(E_max) { }
+
+    float test(
+        const MainState& system,
+        unsigned test_count,
+        unsigned seed) const {
+        default_random_engine reng(seed);
+        uniform_int_distribution<int>dstr(E_min, E_max);
+
+        unsigned good = 0;
+        for (unsigned cnt = 0; cnt != test_count; ++cnt)
+        {
+            if (system.contains(dstr(reng)))
+            {
+                ++good;
+            }
+        }
+        return
+            static_cast<float>(good)
+            / static_cast<float>(test_count);
+    }
+
+
+};
+
+
+/*int main() {
+    DiscreteState d(0);
+    SegmentState s(0, 100);
+    ProbabilityTest pt(-1000, 1000);
+    cout << pt.test(d, 20000, 1) << endl;
+    cout << pt.test(s, 20000, 1) << endl;
+}*/
+int main()
+{
+    // Задание 2.1-2.2
+    const DiscreteState d(1); //c
+    const SegmentState s(10, 10); //c
+    ProbabilityTest pt(-1000, 1000); //c
+    random_device rd;
+
+    /*const vector<int> test_counts = {500, 1000, 5000,
+                                    10000, 20000, 30000, 50000, 
+                                    100000, 500000, 1000000 };*/
+    int seed = rd() % 100;
+
+    cout << "Test_count;    " << "Prob-D;   " << "Prob-S;" << endl;
+    for (int i = 1; i < 9*pow(10,4); i += 10)
+    {
+        cout << i << ";    ";
+        float sumd = 0;
+        for (int j = 0; j < pow(10, 2); j++)
+        {
+            sumd += pt.test(d, i, seed);
+            seed += 10;
+        }
+        float resd = sumd / pow(10, 2);
+        cout << resd << ";    ";
+
+        float sums = 0;
+        for (int j = 0; j < pow(10, 2); j++)
+        {
+            sums += pt.test(s, i, seed);
+            seed += 10;
+        }
+        float ress = sums / pow(10, 2);
+        
+        cout << ress << ";" << endl;
+    }
+
+    // Задание 2.3
+    const SegmentState s1(1, 5);
+    const DiscreteState d1(100);
+    const DiscreteState d2(300);
+    const DiscreteState d3(-900);
+    const DiscreteState d4(-200);
+    const DiscreteState d5(750);
+    cout << pt.test(d1, 1000000, rd() % 100) +
+        pt.test(d2, 1000000, rd() % 100) +
+        pt.test(d3, 1000000, rd() % 100) +
+        pt.test(d4, 1000000, rd() % 100) +
+        pt.test(d5, 1000000, rd() % 100) << endl;
+
+    cout << pt.test(s1, 1000000, rd() % 100) << endl;
+    return 0;
+}
